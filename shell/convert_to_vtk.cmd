67af2a71e7b0e1de21c3798d77364ac76bc8d96b
jing <jing_local@163.com>
2021-04-22 18:55:16 +0800

    update convert to vtk

--- shell/convert_to_vtk.cmd
+++ shell/convert_to_vtk.cmd
@@ -1,3 +1,10 @@
 @echo off
 
+set argC=0
+for %%x in (%*) do Set /A argC+=1
+
+if %argC% NEQ 1 ( 
+	 echo please  specify the input directory
+	 exit /b
+)
 start pvbatch %~dp0convert_to_vtk.py %~dpnx1
